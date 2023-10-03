c44b6248eb9bfa9c59a5dd93e1aca3d5fe669da2
Giovane Seberino <104853803+giovaseberino@users.noreply.github.com>
2023-10-02 22:03:48 -0300

    Add files via upload

---
+++ comando_os.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("cls")
+
+print('Testando')
+print('o')
+print('envio de')
+print('comando')
+print('para o SO utilizando o modulo os')
+
+os.system("mkdir pteste1000")
+
+
+
